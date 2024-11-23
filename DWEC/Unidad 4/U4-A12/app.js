5780132df823d5d9c474b264d53a93a890054ae6
agalan8 <alejandrogalancaballero@hotmail.com>
2024-11-23 17:35:00 +0100

    Actividades 12 y 13 comentadas

--- DWEC/Unidad 4/U4-A12/app.js
+++ DWEC/Unidad 4/U4-A12/app.js
@@ -1,26 +1,39 @@
 
+// Creo el Array que contendrá los artículos
 listaArticulos = [];
 
 const actualizarLista = () => {
+    // Para actualizar la lista simplemente imprimo de nuevo el array
     document.getElementById('lista').innerHTML = listaArticulos;
 }
 
 function anadirArticulo() {
+    // Cojo el articulo que se introduce en el input
     articulo = document.getElementById('articulo').value;
+    // Lo añado al final del array
     listaArticulos.push(articulo);
+    // Llamo a la funcion que me actualiza la lista
     actualizarLista();
 }
 
 function eliminarPrimerArticulo(){
+    // Elimino el primer articulo del array
     listaArticulos.shift();
+    // Actualizo la lista
     actualizarLista();
 }
 
 function eliminarUltimoArticulo(){
+    // Elimino el último articulo del array
     listaArticulos.pop();
+    //Actualizo la lista
     actualizarLista();
 }
 
 function mostrarArticulos(){
+    /* Para mostrar los 3 primeros artículos uso el método slice indicando
+    que empiece por la posición 0 (primera posición) hasta la posición 3
+    como la posición final de este método no se incluye devolverá las
+    posiciones 0, 1 y 2 (primera, segunda y tercera posición).*/
     document.getElementById('tresArticulos').innerHTML = listaArticulos.slice(0,3);
 }
--- DWEC/Unidad 4/U4-A13/app.js
+++ DWEC/Unidad 4/U4-A13/app.js
@@ -1,37 +1,54 @@
 
+// Creo el array vacío
 listaTareas = [];
 
 const actualizarLista = () =>{
+    // Cada vez que actualizo la lista, elimino lo que se imprimión anteriormente
     document.getElementById('tareas').innerHTML = "";
+    // Creo la varuable num para numerar las tareas
     num = 1;
+    // Por cada tarea en el array la imprimo
     listaTareas.forEach((function(tarea){
         document.getElementById('tareas').innerHTML += `<p>${num}. ${tarea.nombre} - Estado: ${tarea.estado} </p>`;
+        // Aumento el número de la tarea para la siguiente
         num++;
     }))
 }
 
 function anadirTarea(){
+    // Obtengo el nombre de la tarea
     nombre = document.getElementById('tarea').value;
+    // Creao una nueva tarea con el nombre anterior y por defecto el estado: pendiente
     tarea = {nombre: nombre, estado: "pendiente"};
+    // Añado la tarea al final del array
     listaTareas.push(tarea);
+    // Actualizo la lista de tareas
     actualizarLista();
 }
 
 function completarTarea(){
+    // Obtengo el número de la tarea que se quiere completar
     indice = document.getElementById('indiceC').value;
+    // Resto 1 para que coincida con la posición de la tarea en el array
     indice = indice - 1;
+    // Compruebo si el índice se encuentra en el array
     if(indice >= 0 && indice < listaTareas.length){
+        // Si se encuentra cambio el estado de la tarea a completada
         listaTareas[indice].estado = "completada"
+        // Actualizo la lista
         actualizarLista();
     } else {
+        // Si no se encuentra muestro un alert informando
         alert("¡Esa tarea no esta en la lista!")
     }
 }
 
+// Esta función hace lo mismo que la anterior pero haciendo uso del método splice
 function eliminarTarea(){
     indice = document.getElementById('indiceE').value;
     indice = indice - 1;
     if(indice >= 0 && indice < listaTareas.length){
+        // Uso el método splice para eliminar 1 tarea en el índice indicado
         listaTareas.splice(indice, 1);
         actualizarLista();
     } else {
@@ -40,23 +57,29 @@ function eliminarTarea(){
 }
 
 function completadas(){
+    // Elimino las tareas que se muestran para mostrar solo las completadas
     document.getElementById('tareas').innerHTML = "";
     num = 1;
+    // Por cada tarea en la lista hago lo siguiente
     listaTareas.forEach((function(tarea){
+        // Compruebo si el estado de la tarea es completada
         if(tarea.estado == "completada"){
+        // Si lo es, imprimo la tarea
         document.getElementById('tareas').innerHTML += `<p>${num}. ${tarea.nombre} - Estado: ${tarea.estado} </p>`;
         num++;
         }
     }))
 }
 
+// Esta función hace lo mismo que la anterior pero haciendo uso del método filter
 function pendientes(){
     document.getElementById('tareas').innerHTML = "";
     num = 1;
-    listaTareas.forEach((function(tarea){
-        if(tarea.estado == "pendiente"){
+    // Guardo en una variable la tareas que cumplen con la condicion (estado === pendiente) haciendo uso del método filter
+    tareasPendientes = listaTareas.filter(tarea => tarea.estado === "pendiente");
+    // Por cada tarea en el array creado anteriormente la imprimo
+    tareasPendientes.forEach(tarea =>{
         document.getElementById('tareas').innerHTML += `<p>${num}. ${tarea.nombre} - Estado: ${tarea.estado} </p>`;
         num++;
-        }
-    }))
+    });
 }
