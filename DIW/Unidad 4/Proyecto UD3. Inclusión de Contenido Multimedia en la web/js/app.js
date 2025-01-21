8975204e03ac1225e6f1594706e1fe50c4c42cba
Alejandro Galán <alejandrogalancaballero@hotmail.com>
2025-01-21 19:35:06 +0100

    Carrusel

--- DIW/Unidad 4/Proyecto UD3. Inclusión de Contenido Multimedia en la web/js/app.js
+++ DIW/Unidad 4/Proyecto UD3. Inclusión de Contenido Multimedia en la web/js/app.js
@@ -9,56 +9,63 @@ album.forEach((cadaAlbum, i)=>{
 
         activar = true;
 
-        if(tab[i].classList.contains('activo')){
+        if(tab[i].classList.contains('tab-activo')){
             activar = false;
         }
 
         album.forEach(( cadaAlbum, i)=>{
-            tab[i].classList.remove('activo');
+            tab[i].classList.remove('tab-activo');
+
         })
 
         if(activar){
-            tab[i].classList.add('activo');
+            var carrusel = tab[i].querySelector('.carrusel .imagenes-carrusel');
+            tab[i].classList.add('tab-activo');
+            carrusel.classList.add('carrusel-activo');
+
+
+            var activo = 0;
+            var imagenes = tab[i].querySelectorAll('.carrusel .imagenes-carrusel .img-carrusel');
+            var siguiente = tab[i].querySelector('#siguiente');
+            var atras = tab[i].querySelector('#atras');
+            var puntos = tab[i].querySelectorAll('.carrusel .puntos li');
+            var longitudImagenes = imagenes.length - 1;
+
+            recargarCarrusel();
+
+
+            siguiente.onclick = function(){
+                activo = activo + 1 <= longitudImagenes ? activo + 1 : 0;
+                recargarCarrusel();
+            }
+
+            atras.onclick = function(){
+                activo = activo - 1 >= 0 ? activo - 1 : longitudImagenes;
+                recargarCarrusel();
+            }
+
+            function recargarCarrusel(){
+
+                carrusel.style.left = -imagenes[activo].offsetLeft + 'px';
+
+                var ultimo_punto_activo = tab[i].querySelector('.carrusel .puntos li.activo');
+                ultimo_punto_activo.classList.remove('activo');
+                puntos[activo].classList.add('activo');
+
+            }
+
+
+
+            puntos.forEach((li, key) => {
+                li.addEventListener('click', ()=>{
+                     activo = key;
+                     recargarCarrusel();
+                })
+            })
+            window.onresize = function(event) {
+                recargarCarrusel();
+            };
+
         }
     })
 })
-
-let carrusel = document.querySelector('.carrusel .imagenes-carrusel');
-let imagenes = document.querySelectorAll('.carrusel .imagenes-carrusel .img-carrusel');
-let siguiente = document.getElementById('siguiente');
-let atras = document.getElementById('atras');
-let puntos = document.querySelectorAll('.carrusel .puntos li');
-
-let longitudImagenes = imagenes.length - 1;
-let activo = 0;
-siguiente.onclick = function(){
-    activo = activo + 1 <= longitudImagenes ? activo + 1 : 0;
-    recargarCarrusel();
-}
-atras.onclick = function(){
-    activo = activo - 1 >= 0 ? activo - 1 : longitudImagenes;
-    recargarCarrusel();
-}
-let refreshInterval = setInterval(()=> {siguiente.click()}, 3000);
-function recargarCarrusel(){
-    carrusel.style.left = -imagenes[activo].offsetLeft + 'px';
-    //
-    let ultimo_punto_activo = document.querySelector('.carrusel .puntos li.activo');
-    ultimo_punto_activo.classList.remove('activo');
-    puntos[activo].classList.add('activo');
-
-    clearInterval(refreshInterval);
-    refreshInterval = setInterval(()=> {siguiente.click()}, 3000);
-
-
-}
-
-puntos.forEach((li, key) => {
-    li.addEventListener('click', ()=>{
-         activo = key;
-         recargarCarrusel();
-    })
-})
-window.onresize = function(event) {
-    recargarCarrusel();
-};
