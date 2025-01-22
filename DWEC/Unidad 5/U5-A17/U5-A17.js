a6337998443e0bea0dd05fac34d411a44b636b1d
Alejandro Galán <alejandrogalancaballero@hotmail.com>
2025-01-22 17:29:43 +0100

    Cliente

---
+++ DWEC/Unidad 5/U5-A17/U5-A17.js
@@ -0,0 +1,76 @@
+
+window.addEventListener('load', () =>{
+
+    document.getElementById('bienvenida').innerHTML = "<h1>BIENVENID@</h1>";
+
+    setTimeout(function() {
+        document.getElementById('bienvenida').innerHTML = "";
+    }, 3000);
+});
+
+var nombre = document.getElementById('nombre');
+var color = document.getElementById('color');
+var notificaciones = document.getElementById('notificaciones');
+var boton_enviar = document.getElementById('enviar');
+
+nombre.addEventListener('keyup', ()=>{
+    document.getElementById('impr_nombre').innerHTML = nombre.value;
+})
+
+color.addEventListener('change', ()=> {
+    document.getElementById('impr_color').style.backgroundColor = color.value;
+})
+
+notificaciones.addEventListener('change', () => {
+
+    if(notificaciones.checked){
+        document.getElementById('impr_notificaciones').innerHTML = "SÍ";
+    } else {
+        document.getElementById('impr_notificaciones').innerHTML = "NO";
+    }
+})
+
+boton_enviar.addEventListener('mouseenter', () => {
+    boton_enviar.style.backgroundColor = "#45a049";
+    boton_enviar.style.transform = "scale(1.05)";
+})
+
+boton_enviar.addEventListener('mouseleave', () => {
+    boton_enviar.style.backgroundColor = "#4CAF50";
+    boton_enviar.style.transform = "scale(1)";
+})
+
+boton_enviar.addEventListener('click', (event) =>{
+
+    if(validarNombre() & validarColor()){
+    } else {
+        event.preventDefault();
+    }
+
+})
+
+function validarNombre(){
+
+    document.getElementById('error_nombre').innerHTML = "";
+    nombre.style.backgroundColor = "";
+
+    if(nombre.value == undefined || nombre.value == ""){
+        document.getElementById('error_nombre').innerHTML = "El nombre es incorrecto";
+        nombre.style.backgroundColor = "lightcoral";
+        return false;
+    }
+
+    return true;
+}
+
+function validarColor(){
+
+    document.getElementById('error_color').innerHTML = "";
+
+    if(color.value == "null"){
+        document.getElementById('error_color').innerHTML = "Debe seleccionar un color";
+        return false;
+    }
+
+    return true;
+}
